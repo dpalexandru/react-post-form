d9fd3364dac108532cd11b612029c7909e06b0ef
dpalexandru <alexpoian91@hotmail.com>
2025-09-03 15:28:39 +0200

    feat: add static form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Form, Button } from "react-bootstrap";
+
+
+
+
 
 
 function App() {
@@ -7,8 +12,41 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1>hello</h1>
-      </div>
+        <h1>Enter a new post</h1>
+        <Form>
+
+          {/* Campo Nome */}
+          <Form.Group className="mb-3" name="author">
+            <Form.Label>Author</Form.Label>
+            <Form.Control type="text" placeholder="Author name" />
+          </Form.Group>
+
+          {/* Campo titolo */}
+          <Form.Group className="mb-3" name="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Title" />
+          </Form.Group>
+
+          {/* Campo Body */}
+          <Form.Group className="mb-3" controlId="body">
+            <Form.Label>Body</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Enter your text here. . ."
+            />
+          </Form.Group>
+          {/* Checkbox */}
+          <Form.Group className="mb-3" controlId="formCheckbox">
+            <Form.Check type="checkbox" label="Pubblic" />
+          </Form.Group>
+
+          {/* Bottone Submit */}
+          <Button variant="primary" type="submit">
+            Invia
+          </Button>
+        </Form>
+      </div >
     </>
   )
 }
