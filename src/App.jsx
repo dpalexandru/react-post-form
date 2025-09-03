038e9021df589689a84b4c5aeb764e243733bf97
dpalexandru <alexpoian91@hotmail.com>
2025-09-03 15:13:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1>hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
